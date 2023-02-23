a415cb02d0a7a131ac86fecad5c29b10bb232e25
Jakarin-Jojo <jakarin.chujan@gmail.com>
2023-02-23 23:32:15 +0700

    Creating the question model and preparing the questions

---
+++ app/src/main/java/eu/tutorials/quizapp/Constants.kt
@@ -0,0 +1,109 @@
+package eu.tutorials.quizapp
+
+object Constants {
+    fun getQuestions():ArrayList<Question>{
+        val questionList = ArrayList<Question>()
+
+        val que1 = Question(
+            1,
+            "What country does this flag belong to",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina", "Australia",
+            "Armenia", "Austria",
+            1
+        )
+        questionList.add(que1)
+
+        val que2 = Question(
+            2,
+            "What country does this flag belong to",
+            R.drawable.ic_flag_of_australia,
+            "Argentina", "Australia",
+            "Armenia", "Austria",
+            2
+        )
+        questionList.add(que2)
+
+        val que3 = Question(
+            3,
+            "What country does this flag belong to",
+            R.drawable.ic_flag_of_belgium,
+            "Argentina", "Australia",
+            "Belgium", "Austria",
+            3
+        )
+        questionList.add(que3)
+
+        val que4 = Question(
+            4,
+            "What country does this flag belong to",
+            R.drawable.ic_flag_of_brazil,
+            "Argentina", "Australia",
+            "Belgium", "Brazil",
+            4
+        )
+        questionList.add(que4)
+
+        val que5 = Question(
+            5,
+            "What country does this flag belong to",
+            R.drawable.ic_flag_of_denmark,
+            "Denmark", "Fiji",
+            "Germany", "India",
+            1
+        )
+        questionList.add(que5)
+
+        val que6 = Question(
+            6,
+            "What country does this flag belong to",
+            R.drawable.ic_flag_of_fiji,
+            "Denmark", "Fiji",
+            "Germany", "India",
+            2
+        )
+        questionList.add(que6)
+
+        val que7 = Question(
+            7,
+            "What country does this flag belong to",
+            R.drawable.ic_flag_of_germany,
+            "Denmark", "Fiji",
+            "Germany", "India",
+            3
+        )
+        questionList.add(que7)
+
+        val que8 = Question(
+            8,
+            "What country does this flag belong to",
+            R.drawable.ic_flag_of_india,
+            "Denmark", "Fiji",
+            "Germany", "India",
+            4
+        )
+        questionList.add(que8)
+
+        val que9 = Question(
+            9,
+            "What country does this flag belong to",
+            R.drawable.ic_flag_of_kuwait,
+            "Denmark", "Fiji",
+            "Kuwait", "India",
+            3
+        )
+        questionList.add(que9)
+
+        val que10 = Question(
+            10,
+            "What country does this flag belong to",
+            R.drawable.ic_flag_of_new_zealand,
+            "Denmark", "Fiji",
+            "New zealand", "India",
+            3
+        )
+        questionList.add(que10)
+
+        return questionList
+    }
+}
--- app/src/main/java/eu/tutorials/quizapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/quizapp/MainActivity.kt
@@ -1,11 +1,28 @@
 package eu.tutorials.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        var nameTxt = findViewById<EditText>(R.id.nameTxt)
+        btnStart.setOnClickListener {
+
+            if(nameTxt.text.isEmpty()){
+                Toast.makeText(this, "Please enter your name", Toast.LENGTH_LONG).show()
+            }else{
+                val intent:Intent = Intent(this, QuizQuestionsActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
     }
 }
